9331be1affe84e4272345a4c99d1be8e8df25710
aishengyu <134182371+aishengyu@users.noreply.github.com>
2023-09-17 01:50:44 +0000

    管理者ユーザ、一般ユーザ対応

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,10 +1,23 @@
 import styled from "styled-components";
-
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
+import { useNavigate } from "react-router-dom";
 
 export const Top = () => {
+
+    const navigate = useNavigate();
+
+    const onClickAdmin = () => navigate("/users");
+    const onClickGeneral = () => alert("onClickGeneral");
+
     return (
         <Scontainer>
             <h2>TOPページです</h2>
+
+            <SecondaryButton onClick={onClickAdmin}>管理者ユーザ</SecondaryButton>
+            <br />
+            <br />
+            <SecondaryButton onClick={onClickGeneral}>一般ユーザ</SecondaryButton>
+
         </Scontainer>
     );
 }
